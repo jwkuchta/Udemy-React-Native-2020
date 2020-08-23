61704c0a26ed94ce9cdcaee786578de98bdf4402
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-23 14:13:35 -0400

    list items display on screen

--- App.js
+++ App.js
@@ -1,28 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, View, Button } from 'react-native';
+import { StyleSheet, TextInput, View, Button, Text, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
 
-  const [ output, setOutput ] = useState('Open up App.js to start working on your project')
+  const [ input, setInput ] = useState('')
+  const [ goals, setGoals ] = useState([])
+
+  const inputHandler = (input) => {
+    setInput(input)
+  }
+
+  const addGoalHandler = () => {
+    setGoals(goals => [...goals, { key: Math.random().toString(), value: input }])
+  }
   return (
     <View style={styles.screen}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between'}}>
+      <View style={styles.inputContainer}>
         <TextInput 
         placeholder='Enter your goal' 
-        style={{ width: '80%', borderBottomColor: 'grey', borderBottomWidth: 1, padding: 10 }} 
+        style={styles.input} 
+        onChangeText={inputHandler}
+        value={input}
         />
-        <Button title='ADD'/>
+        <Button title='ADD' onPress={addGoalHandler}/>
       </View>
-      <View>
+      <FlatList 
+      data={goals} 
+      renderItem={ itemData => (
+      <View style={styles.listItem} key={itemData.item.key}>
+        <Text>{itemData.item.value}</Text>
       </View>
+      )}>
+      </FlatList>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50
+  screen: { 
+    padding: 50 
+  },
+  inputContainer: { 
+    flexDirection: 'row', 
+    justifyContent: 'space-between',
+    alignItems: "center"
+  },
+  input: { 
+    width: '80%', 
+    borderBottomColor: 'grey', 
+    borderBottomWidth: 1, 
+    padding: 10 
+  },
+  listItem: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 })
 
