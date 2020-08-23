146664655dfa97f49fe2a9a8b8fec85bbd9bd898
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-23 10:49:40 -0400

    added initial styling

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function App() {
 
   const [ output, setOutput ] = useState('Open up App.js to start working on your project')
   return (
-    <View style={styles.container}>
-      <Text>{output}</Text>
-      <Button title="Change Text" onPress={() => setOutput('The text will not change')}/>
+    <View style={styles.screen}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between'}}>
+        <TextInput 
+        placeholder='Enter your goal' 
+        style={{ width: '80%', borderBottomColor: 'grey', borderBottomWidth: 1, padding: 10 }} 
+        />
+        <Button title='ADD'/>
+      </View>
+      <View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  screen: {
+    padding: 50
+  }
+})
+
